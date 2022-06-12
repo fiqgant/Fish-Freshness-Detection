a31b46c496f942e25610a8dab2b4330a1b2b2113
Taufiqurrahman <64482868+fiqgant@users.noreply.github.com>
2022-06-13 00:22:54 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
 mkdir -p ~/.streamlit/
 
 echo "\
+[general]\n\
+email = \"fiqgant@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
-" > ~/.streamlit/config.toml 
+" > ~/.streamlit/config.toml
